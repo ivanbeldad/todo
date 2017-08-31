9495acf97e828fbe9ccbe7cb3f2cd528b74a917b
Iván de la Beldad Fernández <ivandelabeldad@gmail.com>
2017-08-31 05:47:44 +0200

    Added Note model

---
+++ src/main/java/com/rackian/todo/model/Note.java
@@ -0,0 +1,50 @@
+package com.rackian.todo.model;
+
+import java.util.Objects;
+
+public class Note {
+
+    private String title;
+    private String content;
+
+    public Note() {
+        title = "";
+        content = "";
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(title, note.title) &&
+                Objects.equals(content, note.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, content);
+    }
+
+}
---
+++ src/test/java/com/rackian/todo/model/NoteSpec.java
@@ -0,0 +1,65 @@
+package com.rackian.todo.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class NoteSpec {
+
+    private Note emptyNote;
+    private Note note1;
+    private Note note2;
+
+    @Before
+    public void setUp() throws Exception {
+        emptyNote = new Note();
+        note1 = new Note("title", "content");
+        note2 = new Note("title", "content");
+    }
+
+    @Test
+    public void whenEmptyConstructorThenTitleAndContentAreEmpty() throws Exception {
+        assertThat(emptyNote.getTitle(), is(""));
+        assertThat(emptyNote.getContent(), is(""));
+    }
+
+    @Test
+    public void whenTitleAndContentConstructorThenTitleAndContentAreFilled() throws Exception {
+        Note note = new Note("My title", "My content");
+        assertThat(note.getTitle(), is("My title"));
+        assertThat(note.getContent(), is("My content"));
+    }
+
+    @Test
+    public void whenTitleIsSettedThenItChanges() throws Exception {
+        emptyNote.setTitle("New title");
+        assertThat(emptyNote.getTitle(), is("New title"));
+    }
+
+    @Test
+    public void whenContentIsSettedThenItChanges() throws Exception {
+        emptyNote.setContent("New content");
+        assertThat(emptyNote.getContent(), is("New content"));
+    }
+
+    @Test
+    public void whenNotesWithSameTitleAndContentAreComparedThenTheyAreEquals() throws Exception {
+        assertThat("Note1 equals note2", note1.equals(note2));
+        assertThat("Note2 equals note1", note2.equals(note1));
+        assertThat(note1.hashCode(), is(note2.hashCode()));
+    }
+
+    @Test
+    public void whenNotesWithDifferentTitleOrContentAreComparedThenTheyAreNotEquals() throws Exception {
+        note2.setTitle("title different");
+        assertThat("Note1 not equals note2 if different title", !note1.equals(note2));
+        assertThat(note1.hashCode(), not(note2.hashCode()));
+        note2.setTitle("title");
+        note2.setContent("content different");
+        assertThat("Note2 not equals note1 if different content", !note2.equals(note1));
+        assertThat(note1.hashCode(), not(note2.hashCode()));
+    }
+
+}
